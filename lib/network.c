362c41281429eacd54203d8db4a766dbc8eed2ca
iasebsil83 <i.a.sebsil83@gmail.com>
2023-07-24 19:34:56 +0200

    Changed LICENSE from GPLv2 to LGPLv2.1

--- lib/network.c
+++ lib/network.c
@@ -22,7 +22,7 @@
 
 
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Network [0.1.1] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Network [0.1.2] ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                                  Network by I.A.
 
         Network is just an utility program that allows you to create and join
@@ -39,6 +39,9 @@
     - Finished implementation of UDP in IPv4.
     - Finished implementation of UDP in IPv6.
 
+    07/01/2023 > [0.1.2] :
+    - Fixed everything in TCP & UDP for both IPv4 & IPv6.
+
     BUGS : .
     NOTES : .
 
@@ -49,18 +52,23 @@
     Let's Code !                                  By I.A.
 *********************************************************************************
     LICENCE :
+
     C_Network
-    Copyright (C) 2021  Sebastien SILVANO
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-    This program is distributed in the hope that it will be useful,
+    Copyright (C) 2023 Sebastien SILVANO
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.
+
     If not, see <https://www.gnu.org/licenses/>.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
